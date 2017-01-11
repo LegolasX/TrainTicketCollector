e1d26d3e40680034aa4a777a52d49da93bc89f6d
LegolasX <Legolas.Invoker@gmail.com>
2017-01-11 21:08:40 +0800

    init

---
+++ 火车票贩子/火车票贩子/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  火车票贩子
+//
+//  Created by Legolas.Invoker on 2017/1/11.
+//  Copyright © 2017年 Chang.Jing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
